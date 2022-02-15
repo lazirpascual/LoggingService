dd8968614ca79a907c64b5f6220add8f6402e544
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-15 16:41:36 -0500

    create header comment for logging service

--- LoggingService/Program.cs
+++ LoggingService/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+FILE                : TestingClient.py
+PROJECT			    : A3 - Network Application Development
+PROGRAMMER		    : Lazir Pascual, Rohullah Noory
+FIRST VERSION       : 2/15/2022
+DESCRIPTION		    :
+*/
+
+using System;
 
 namespace LoggingService
 {
