265afa4ea6c73f3da423a3b050667253a53ff9d8
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-15 16:40:28 -0500

    Create header comment for testing client

---
+++ TestingClient.py
@@ -0,0 +1,6 @@
+#
+# FILE              : TestingClient.py
+# PROJECT			: A3 - Network Application Development
+# PROGRAMMER		: Lazir Pascual, Rohullah Noory
+# FIRST VERSION     : 2/15/2022
+# DESCRIPTION		:
