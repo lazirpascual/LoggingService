9870e24105b20faf2ef149a10388a371ed5a23f2
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-15 16:34:51 -0500

    create directory for logging service

---
+++ LoggingService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
