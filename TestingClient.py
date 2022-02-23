987d955d14fe3fe5c6dc2da4ff0d0510f034a48e
Lazir Pascual <lpascual4579@conestogac.on.ca>
2022-02-23 17:36:54 -0500

    Remove test client from directory

--- TestingClient.py
+++
@@ -1,6 +0,0 @@
-#
-# FILE              : TestingClient.py
-# PROJECT			: A3 - Network Application Development
-# PROGRAMMER		: Lazir Pascual, Rohullah Noory
-# FIRST VERSION     : 2/15/2022
-# DESCRIPTION		:
